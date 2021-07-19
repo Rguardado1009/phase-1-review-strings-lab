8769581fd34266a2f2fba858fa04605969e2e547
Rguardado1009 <rguardado1009@gmail.com>
2021-07-19 11:44:15 -0400

    commit tree ebe2d9d0b6eba31a805a36dd8af384fa72269022

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = 'Ronald';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !` ;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let firstInitial = currentUser[0];
+let restOfName = currentUser.slice(1);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
